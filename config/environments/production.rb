03ebfd5795f8dea3519fe418ce742339df732400
Renan Martins <renanmartins989@gmail.com>
2022-09-10 03:17:34 +0100

    still with problems to get the results on heroku, local is working fine. Need to maybe remove all dependences of mailer and add again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "https://yourpetmatch.herokuapp.com/" }
+  config.action_mailer.default_url_options = { host: "https://melhor-raca.herokuapp.com/" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
