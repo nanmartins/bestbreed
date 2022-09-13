af2278ac9df2c1560f9a42d72f8b5ed2efac7c1f
Renan Martins <renanmartins989@gmail.com>
2022-09-13 04:40:05 +0100

    return user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,13 @@
-# class UserMailer < ApplicationMailer
+class UserMailer < ApplicationMailer
 
-#   # Subject can be set in your I18n file at config/locales/en.yml
-#   # with the following lookup:
-#   #
-#   #   en.user_mailer.infos.subject
-#   #
-#   def infos
-#     @user = params
-#     @result = params[:result]
-#     mail(to: @result.user.email, subject: 'Raça ideal do seu melhor amigo')
-#   end
-# end
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.infos.subject
+  #
+  def infos
+    @user = params
+    @result = params[:result]
+    mail(to: @result.user.email, subject: 'Raça ideal do seu melhor amigo')
+  end
+end
