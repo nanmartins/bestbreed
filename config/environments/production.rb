cc9aee1b593a7c2bd7b211c6391d2a295bec34ee
Renan Martins <renanmartins989@gmail.com>
2022-09-13 04:18:29 +0100

    heroku is chashing after remove the email sender

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.default_url_options = { host: "https://yourpetmatch.herokuapp.com/" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "https://yourpetmatch.herokuapp.com/" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
